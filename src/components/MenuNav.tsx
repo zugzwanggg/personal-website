ec9a4f3a7befa5541082d016f27c8920aadc77a6
Amankos Ansar <zugzwanggg0@gmail.com>
2023-12-24 01:56:29 +0500

    fixed navbar

--- src/components/MenuNav.tsx
+++ src/components/MenuNav.tsx
@@ -5,7 +5,7 @@ import {IBurger} from "./Header"
 
 export const MenuNav:FC<IBurger> = ({burger,setBurger}) => {
   return (
-    <nav className={`${burger ? 'translate-x-0 ' : '-translate-x-full'} transition ease-in-out duration-500 p-20 fixed top-20 h-screen w-screen bg-slate-50`}>
+    <nav className={`${burger ? 'translate-x-0 ' : '-translate-x-full'} transition ease-in-out duration-500 p-20 fixed top-16 h-screen w-screen bg-slate-50`}>
       <ul className="flex flex-col gap-6 justify-center items-center">
         <li onClick={()=>setBurger(false)}><a className="hover:bg-slate-300 p-6" href="#about">About</a></li>
         <li onClick={()=>setBurger(false)}><a className="hover:bg-slate-300 p-6" href="#projects">Projects</a></li>
